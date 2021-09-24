db069d65dfa828a5b9690663ce5f4f30ae16b270
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-09-25 01:20:27 +0900

    add router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from"./components/App";
+import firebase from "./firebase";
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
