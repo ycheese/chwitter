a6b88167b9cfb5fe0b2e885aeb7498b9a2267cbd
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-10-01 01:20:06 +0900

    add logout

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,19 @@
-const Profile = () =>  <span>Profile</span>;
+import { authService } from "fbase";
+//import { useHistory } from "react-router-dom";
+
+const Profile = () => {
+    //const history = useHistory();
+
+    const onLogoutClick = () => {
+        authService.signOut();
+        //history.push("/");
+    };
+
+    return (
+        <>
+            <button onClick={onLogoutClick}>Log Out</button>
+        </>
+    );
+};
 
 export default Profile;
