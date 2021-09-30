ec02488a1248bbac4e17d98d2f6d0699da4ff1a0
ychxexn <81912261+ychxexn@users.noreply.github.com>
2021-09-30 21:12:53 +0900

    add firebase auth

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,5 +15,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export default app;
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
